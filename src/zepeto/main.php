e20aab6fd8e78c8cdddbacb11014e5663757d52c
zepeto <61928471+mazepeto@users.noreply.github.com>
2020-07-11 13:31:42 +0900

    Update main.php

--- src/zepeto/main.php
+++ src/zepeto/main.php
@@ -46,7 +46,7 @@ $player->setHealth($player->getMaxHealth());
         $player->addTitle("§l§c사망", "§r§e10초후 움직일 수 있습니다", 30, 200, 20);
         break;
         case 7:
-        $player->addTitle("§lßc사망", "§r§e20초후 움직일 수 있습니다", 30, 400, 20);
+        $player->addTitle("§l§c사망", "§r§e20초후 움직일 수 있습니다", 30, 400, 20);
         }
     }
 
