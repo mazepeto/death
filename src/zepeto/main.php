1d3cdec241bd7a80a7a439d804efb2d66a24bc80
zepeto <61928471+mazepeto@users.noreply.github.com>
2020-07-11 23:24:14 +0900

    Update main.php

--- src/zepeto/main.php
+++ src/zepeto/main.php
@@ -27,28 +27,35 @@ $this->title
    }
 }
   
-  public function title(player $playee){
+  public function title(player $player){
     switch (mt_rand(1,7)){
         case 1:
         $player->addTitle("§l§c사망", "§r§e5초후 움직일 수 있습니다", 30, 100, 20);
+        $player->addEffect(new EffectInstance(Effect::getEffect(15),100, 1, false));
         break; 
         case 2:
         $player->addTitle("§l§c사망", "§r§e10초후 움직일 수 있습니다", 30, 200, 20);
+        $player->addEffect(new EffectInstance(Effect::getEffect(15),200, 1, false));
         break;
         case 3:
         $player->addTitle("§l§c사망", "§r§e15초후 움직일 수 있습니다", 30, 300, 20);
+        $player->addEffect(new EffectInstance(Effect::getEffect(15),300, 1, false));
         break;
         case 4:
         $player->addTitle("§l§c사망", "§r§e5초후 움직일 수 있습니다", 30, 100, 20);
+        $player->addEffect(new EffectInstance(Effect::getEffect(15),100, 1, false));
         break;
         case 5:
         $player->addTitle("§l§c사망", "§r§e5초후 움직일 수 있습니다", 30, 100, 20);
+        $player->addEffect(new EffectInstance(Effect::getEffect(15),100, 1, false));
         break;
         case 6:
         $player->addTitle("§l§c사망", "§r§e10초후 움직일 수 있습니다", 30, 200, 20);
+        $player->addEffect(new EffectInstance(Effect::getEffect(15),200, 1, false));
         break;
         case 7:
         $player->addTitle("§l§c사망", "§r§e20초후 움직일 수 있습니다", 30, 400, 20);
+        $player->addEffect(new EffectInstance(Effect::getEffect(15),400, 1, false));
         }
     }
 
